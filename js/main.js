f8f8a8a108b91b315981fa9c64ba1d83f3ca31f5
itscopebd <itscopebd@gmail.com>
2023-03-02 21:00:45 +0600

    Load data from api

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log("rofiq")
+
+const defaultData=6;
+// load api data 
+const loadData=(defaultData)=>{
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+    .then(res=> res.json())
+    .then(data=> showLimitedData(data,defaultData))
+    .catch(err=> console.log(err))
+}
+
+
+loadData(defaultData);
