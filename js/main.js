d8e8b04462a5696249eee67f2f3190a8b50ac3f0
itscopebd <itscopebd@gmail.com>
2023-03-03 16:57:28 +0600

    Single data load in the modal

--- js/main.js
+++ js/main.js
@@ -23,15 +23,15 @@ const showLimitedData=(data,defaultData)=>{
     }else{
       seeMoreBtn.classList.add("d-none")
     }
-
+console.log(data)
     const loadData= document.getElementById("loadCard");
     loadData.innerHTML="";
     data.forEach(element => {
-        const {features,image,name,published_in,description}=element;
+        const {features,image,name,published_in}=element;
 
         // features value iteration 
       const featureData=features.map(data=> `<li>  ${data} </li>`);
-console.log(featureData)
+
        const div = document.createElement("div");
        div.classList.add("col-12","col-md-6","col-lg-4");
        div.innerHTML=`
@@ -40,7 +40,6 @@ console.log(featureData)
          <div class="card-body">
            <h5 class="card-title">Features</h5>
            <ol>${featureData.join(" ")}</ol>
-           <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
          </div>
          <div class="card-footer d-flex justify-content-between align-items-center">
            <div>
@@ -103,6 +102,27 @@ console.log(error)
 //  single data  and modal show
 const showSingleData=data=>{
 const load_modal= document.getElementById("load_modal");
+
+const {image_link,description,features,integrations,pricing,accuracy}=data.data;
+
+// features data iteration 
+featureData=[];
+for (const key in features) {
+  const li= `<li>${features[key].feature_name}</li>`;
+  featureData.push(li);
+  
+}
+if (accuracy.score ==null) {
+const badgeHide=document.getElementById("badge");
+badgeHide.classList.add("d-none")
+  
+}
+// integrations data iteration 
+const integrationsData=integrations.map(value=>{
+return `<li>${value}</li>`
+})
+
+console.log(data)
 load_modal.innerHTML=`
 
 <div class="modal-content">
@@ -111,7 +131,49 @@ load_modal.innerHTML=`
         data-bs-dismiss="modal" aria-label="Close"></button>
 </div>
 <div class="modal-body">
-    ...
+    <div class="row gap-2 justify-content-center">
+    <div class="col-11 col-lg-6 py-3 order-1 order-md-1 order-lg-0 border border-1 border-danger custom__bg">
+    
+    <div class="card-title">
+    <b>${description}</b>
+    </div>
+
+    <div class="d-flex justify-content-between gap-2 text-center flex-wrap ">
+    <div class="bg-white p-3 rounded">
+        <p class="text-success">${pricing[0] ? pricing[0].price :"Free of Cost/"} <br> ${ pricing[0].plan }</p>
+
+    </div>
+    <div class="bg-white p-3 rounded">
+    <p class="text-wornning">${pricing[1] ? pricing[1].price :"Free of Cost/"} <br> ${ pricing[1].plan }</p>
+
+    </div>
+    <div class="bg-white p-3 rounded">
+    <p class="text-wornning">${pricing[2] ? pricing[2].price :"Free of Cost/"} <br> ${ pricing[2].plan }</p>
+
+    </div>
+</div>
+
+
+  <div class="d-flex justify-content-between">
+            <div>
+                <h4>Feature</h4>
+                <ul>${featureData.join(" ")}</ul>
+            </div>
+            <div>
+             <h4>Integrations</h4>
+             <ul>${integrationsData.length >0 ? integrationsData.join(" "):"No data Found"}</ul>
+            </div>
+        </div>
+
+    </div>
+    <div class="col-11 col-lg-5 py-3 order-0 order-lg-1 order-md-0 border border-1 border-danger position-relative">
+    <img src="${image_link[0]}" style="width:100%">
+    <span class="badge"  id="badge">
+    ${accuracy.score * 100} %
+       </span>
+    </div>
+
+    </div>
 </div>
 
 </div>
